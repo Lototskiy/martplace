57d1e34431386037db82898d7d1173a11d1badfe
Dmytro <dmytro.lototskiy@gmail.com>
2020-04-19 00:32:37 +0300

    products list

--- gulpfile.js
+++ gulpfile.js
@@ -94,6 +94,7 @@ gulp.task("style", function () {
 			//указываем, где брать исходники
 			"node_modules/normalize.css/normalize.css",
 			'node_modules/slick-carousel/slick/slick.css',
+			"node_modules/ion-rangeslider/css/ion.rangeSlider.css",
 			"node_modules/rateyo/src/jquery.rateyo.css",
 		])
 		.pipe(sourcemaps.init())
@@ -112,6 +113,7 @@ gulp.task("script", function () {
 			"node_modules/jquery/dist/jquery.js",
 			'node_modules/slick-carousel/slick/slick.js',
 			"node_modules/rateyo/src/jquery.rateyo.js",
+			"node_modules/ion-rangeslider/js/ion.rangeSlider.js",
 			"node_modules/mixitup/dist/mixitup.js"
 		])
 		.pipe(size())
